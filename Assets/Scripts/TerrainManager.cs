634f9255f333b141ee01e42d786970673f10b78f
Daniel D'Onofrio <dan.donof@gmail.com>
2019-12-14 23:26:27 +1030

    - terrainManager now exists, spawns the starting floor successfully - set up to use TerrainPiece scriptable objects

---
+++ Assets/Scripts/TerrainManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TerrainManager : MonoBehaviour
+{
+	public List<TerrainPiece> groundBank;
+	public List<TerrainPiece> platformBank;
+
+	public List<GameObject> spawnedObjects;
+
+	public float scrollSpeed = 1f;
+	public float scrollIncreaseAmount = 0.05f;
+	public float scrollIncreaseTickRate = 5f;
+
+	public Vector2 groundOffset;
+
+	public float destroyPoint = -50f;
+	public float spawnPoint = 50f;
+
+	// Start is called before the first frame update
+	void Start()
+    {
+		// initialise a set of ground under the player
+		float i = destroyPoint;
+		while (i <= spawnPoint)
+		{
+			// spawn a random ground piece
+			int rng = Random.Range(0, groundBank.Count);
+			GameObject go = Instantiate(groundBank[rng].spawnPrefab, groundOffset, Quaternion.identity); 
+			go.transform.position += new Vector3(i, 0f, 0f); // move it into position
+			i += groundBank[rng].xUnitsOccupied; // move spawn position forward
+		}
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/TerrainPiece.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "TerrainPiece", menuName = "TerrainPiece", order = 1)]
+public class TerrainPiece : ScriptableObject
+{
+	public GameObject spawnPrefab;
+	public float xUnitsOccupied = 0f; // horizontal units this piece occupies
+	public float minPrevYHeight = 0f; // minimum height of previous spawned piece required to access this piece
+}
